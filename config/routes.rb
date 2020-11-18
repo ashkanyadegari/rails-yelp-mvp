9be3dd0cc2cad2f91f8ca73749e27e942fd69a6e
Ashkan Yadegari <ashkan.yadegari@outlook.com>
2020-11-18 14:54:57 +0800

    Yelp Clone

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
+  get 'reviews/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+    collection do
+
+    end
+    member do
+
+    end
+  end
+  resources :reviews, only: [:destroy]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+  # create_table "restaurants", force: :cascade do |t|
+  #   t.string "name"
+  #   t.string "address"
+  #   t.string "phone_number"
+  #   t.string "category"
+  #   t.datetime "created_at", precision: 6, null: false
+  #   t.datetime "updated_at", precision: 6, null: false
+  # end
+
+  if Rails.env.development?
+
+    puts "Deleted all seeds"
+    Restaurant.destroy_all
+    puts "Starting seeds"
+    50.times do
+      restaurant = Restaurant.new(name: Faker::Restaurant.name, category: %w(chinese italian japanese french belgian).sample,
+      address: Faker::Address.street_address, phone_number: Faker::PhoneNumber.cell_phone)
+      restaurant.save
+      puts "#{restaurant.name} has been created"
+    end
+    puts "Seed has ended"
+  end
